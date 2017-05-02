4afd9c679475dbb8f0e45bf0918386ef49e568a9
leantony <chachaantony@gmail.com>
2017-05-02 16:05:02 +0300

    apply danger class properly

--- src/views/tables/buttons/delete.blade.php
+++ src/views/tables/buttons/delete.blade.php
@@ -2,7 +2,7 @@
     <a data-pjax-target="{{ $pjaxTarget }}"
        data-method="DELETE" data-confirm="Are you sure you want to do this?"
        href="{{ $url }}"
-       class="data-remote btn btn-xs" data-toggle="tooltip"
+       class="data-remote btn danger btn-xs" data-toggle="tooltip"
        data-title="{{ $title }}">{{ $name }}</a>
 @else
     <a data-method="DELETE" data-confirm="Are you sure you want to do this?"
